6060b3eb29a25fdbf80782d0cb9fd2ad95cb30d7
Superjun <65696808+ByeongjunPark@users.noreply.github.com>
2023-08-30 19:18:04 +0900

    [Bronze III] Title: 최소, 최대, Time: 552 ms, Memory: 147116 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -0,0 +1,11 @@
+n=int(input())
+arr=list(map(int, input().split()))
+max=arr[0]
+min=arr[0]
+for i in range(len(arr)):
+  if (max<arr[i]):
+    max=arr[i]
+  if (min>arr[i]):
+    min=arr[i]
+
+print("{} {}".format(min, max))
