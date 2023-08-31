36d9bcba708c89035defa95609fcd0d7608efba5
Superjun <65696808+ByeongjunPark@users.noreply.github.com>
2023-08-31 16:08:44 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.c
@@ -0,0 +1,31 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+int main() {
+
+	int n;
+
+	scanf("%d", &n);
+
+	
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= 2 * n; j++) {
+			if (i % 2 != 0) {
+				if (j % 2 != 0)
+					printf("*");
+				else
+					printf(" ");
+			}
+			else {
+				if (j % 2 == 0)
+					printf("*");
+				else
+					printf(" ");
+			}
+		}
+		if (i == n)
+			continue;
+		printf("\n");
+	}
+	
+	return 0;
+}
