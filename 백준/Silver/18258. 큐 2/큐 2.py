4b7595282041a64268ac8f86b75398206da8f880
Byeongjun Park <bjpark0925@gmail.com>
2025-03-12 14:05:17 +0900

    [Silver IV] Title: 큐 2, Time: 1656 ms, Memory: 112508 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+q = deque()
+n = int(sys.stdin.readline())
+for i in range(n):
+    order = sys.stdin.readline().split()
+    if order[0] == "push":
+        q.append(int(order[1]))
+    elif order[0] == "pop":
+        if len(q) == 0:
+          print(-1)
+        else:
+          print(q.popleft())
+    elif order[0] == "size":
+        print(len(q))
+    elif order[0] == "empty":
+        if len(q) == 0:
+          print(1)
+        else:
+          print(0)
+    elif order[0] == "front":
+        if len(q) == 0:
+          print(-1)
+        else:
+          print(q[0])
+    elif order[0] == "back":
+        if len(q) == 0:
+          print(-1)
+        else:
+          print(q[-1])
