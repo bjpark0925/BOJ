a4366375fbbbfa8ca8bb62f3189ef4b294b757a9
Byeongjun Park <bjpark0925@gmail.com>
2025-03-26 15:14:13 +0900

    [Gold V] Title: 회의실 배정, Time: 272 ms, Memory: 60360 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,10 +1,13 @@
 # 가장 이른 종료시간 task부터 greedy하게 넣기
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 meeting_schedule = []
 for i in range(n):
     meeting_schedule.append(list(map(int,input().split())))
 
-meeting_schedule.sort(key=lambda x: (x[1],x[0]))
+meeting_schedule.sort(key=lambda x: (x[1],x[0])) # 1열 기준 정렬 후 0열 기준 정렬
 meeting_end = meeting_schedule[0][1]
 meeting_cnt = 1
 for i in range(1,n):
