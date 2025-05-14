039682c78cacabf6af637aa51016f35cce8e2718
Byeongjun Park <bjpark0925@gmail.com>
2025-05-14 14:39:30 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,5 +1,8 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+
 def dfs(arr):
-    global n, m
     if len(arr) > 1 and arr[-2] > arr[-1]:
         return
     if len(arr) == m:
@@ -13,5 +16,4 @@ def dfs(arr):
 
     return
 
-n, m = map(int, input().split())
 dfs([])
