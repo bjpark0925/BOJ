28fd4f8e37314c9daf1dd05d02c4dde108458434
Byeongjun Park <bjpark0925@gmail.com>
2025-05-19 14:25:32 +0900

    [Gold IV] Title: 공유기 설치, Time: 288 ms, Memory: 41164 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -5,15 +5,14 @@ n, c = map(int, input().split())
 arr = []
 for _ in range(n):
     arr.append(int(input()))
-arr.sort() # 1 2 4 8 9
+arr.sort()
 
 left = 1
 right = (arr[n-1] - arr[0]) // (c - 1) # ideal max length
 result = 0
 while left <= right:
     mid = (left + right) // 2
-    #print(left, mid, right)
-    cur = arr[0]
+    cur = arr[0] # important
     cnt = 1
     for i in range(n):
         if arr[i] - cur >= mid:
