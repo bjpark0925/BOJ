a654657715054ff35288c2c4317d8cf32a8ebdf5
Byeongjun Park <bjpark0925@gmail.com>
2025-09-24 13:28:07 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+#print(arr)
+dp = [0] * (n+1)
+for i in range(n-1, -1, -1):
+    prev = i + arr[i][0]
+    if prev > n:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(arr[i][1] + dp[prev], dp[i+1])
+
+print(dp[0])
