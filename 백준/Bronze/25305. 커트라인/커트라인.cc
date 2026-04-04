f1ecdec6dd6b60c306aad89d0e4ffdb361e72d7a
Byeong-Jun Park <bjpark0925@gmail.com>
2026-04-04 19:24:57 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n, k;
+    cin >> n >> k;
+    
+    vector<int> arr(n);
+    for (int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    
+    sort(arr.begin(), arr.end());
+    reverse(arr.begin(), arr.end());
+    cout << arr[k-1] << endl;
+    
+    return 0;
+}
