3d4f67aa760e04bfaa21b081be1640bf567aa4b5
Byeong-Jun Park <bjpark0925@gmail.com>
2026-04-06 18:49:35 +0900

    [Silver III] Title: queuestack, Time: 40 ms, Memory: 2944 KB -BaekjoonHub

---
+++ 백준/Silver/24511. queuestack/queuestack.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    vector<int> data_st(n);
+    for (int i=0;i<n;i++){
+        cin >> data_st[i];
+    }
+    
+    deque<int> elem;
+    for (int i=0;i<n;i++){
+        int x;
+        cin >> x;
+        if (data_st[i] == 0){
+            elem.push_back(x);
+        }
+    }
+    
+    int m;
+    cin >> m;
+    for (int i=0;i<m;i++){
+        int c;
+        cin >> c;
+        
+        if (!elem.empty()){
+            cout << elem.back();        
+            elem.pop_back();
+            elem.push_front(c);
+        }
+        else{
+            cout << c;
+        }
+        
+        if (i<m-1) cout << " ";
+    }    
+    
+    return 0;
+}
