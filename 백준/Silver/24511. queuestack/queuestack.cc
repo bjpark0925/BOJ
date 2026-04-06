805f83a2f07376746ce129abe00504ad4ce8ab65
Byeong-Jun Park <bjpark0925@gmail.com>
2026-04-06 18:52:14 +0900

    [Silver III] Title: queuestack, Time: 40 ms, Memory: 2944 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.cc
+++ 백준/Silver/24511. queuestack/queuestack.cc
@@ -1,35 +1,36 @@
-#include <iostream>
-#include <vector>
-#include <deque>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main() {
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
+    
     int n;
     cin >> n;
-
-    vector<int> type(n);
-    for (int i = 0; i < n; i++) cin >> type[i];
-
-    deque<int> dq;
-    for (int i = 0; i < n; i++) {
+    vector<int> data_st(n);
+    for (int i=0;i<n;i++){
+        cin >> data_st[i];
+    }
+    
+    deque<int> elem;
+    for (int i=0;i<n;i++){
         int x;
         cin >> x;
-        if (type[i] == 0) dq.push_back(x);  // queue만 저장
+        if (data_st[i] == 0){
+            elem.push_back(x);
+        }
     }
-
+    
     int m;
     cin >> m;
-
-    for (int i = 0; i < m; i++) {
-        int x;
-        cin >> x;
-        dq.push_front(x);            // 앞에 삽입
-        cout << dq.back() << "\n";   // 뒤에서 출력
-        dq.pop_back();               // 뒤에서 삭제
-    }
-
+    for (int i=0;i<m;i++){
+        int c;
+        cin >> c;
+        
+        elem.push_front(c);
+        cout << elem.back();
+        if (i<m-1) cout << " ";
+        elem.pop_back();
+    }    
+    
     return 0;
 }
