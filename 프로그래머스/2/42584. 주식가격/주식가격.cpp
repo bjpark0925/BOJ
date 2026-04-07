2aa91f1c32f2c8982f2dfbc3d7a8cf20836d5e44
Byeong-Jun Park <bjpark0925@gmail.com>
2026-04-07 17:07:51 +0900

    [level 2] Title: 주식가격, Time: 26.55 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -5,6 +5,25 @@
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
+    int n = prices.size();
+    vector<int> answer(n);
+    stack<int> st; // 인덱스 저장
+    
+    // 1 2 3 4 5 3 3
+    for (int i=0;i<n;i++){
+        while (!st.empty() && prices[st.top()] > prices[i]){
+            answer[st.top()] = i - st.top();
+            st.pop();
+        }
+        st.push(i);
+    }
+    
+    while (!st.empty()){
+        answer[st.top()] = n-1-st.top();
+        st.pop();
+    }
+    
+    /*
     vector<int> answer;
     
     for (int i=0;i<prices.size();i++){
@@ -17,6 +36,7 @@ vector<int> solution(vector<int> prices) {
         }
         answer.push_back(cnt);
     }
+    */
     
     return answer;
 }
