093e01bda7ec53dd913d8cf999c2f2d8eec48a21
Byeong-Jun Park <bjpark0925@gmail.com>
2026-05-13 19:22:57 +0900

    Add tree traversal functions in 1991 트리 순회.py
    Implement tree traversal methods: preorder, inorder, and postorder.

---
+++ 백준/섭종후/33. 트리/1991 트리 순회.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+n = int(input())
+graph = defaultdict(list)
+for _ in range(n):
+  p, left, right = input().split()
+  graph[p] = (left, right)
+
+def preorder(root):
+  if root != '.':
+    print(root, end='')
+    preorder(graph[root][0])
+    preorder(graph[root][1])
+
+def inorder(root):
+  if root != '.':
+    inorder(graph[root][0])
+    print(root, end='')
+    inorder(graph[root][1])
+
+def postorder(root):
+  if root != '.':
+    postorder(graph[root][0])
+    postorder(graph[root][1])
+    print(root, end='')
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
